dd4ade1a3baf1fabbd37cc83ced075bcdee655ac
yonatan <yonatanavizov1997@gmail.com>
2022-02-25 21:06:06 +0200

    Test

--- src/com/view/PopUp.java
+++ src/com/view/PopUp.java
@@ -6,10 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class PopUp extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField txtHelloOverThere;
 
 	/**
 	 * Launch the application.
@@ -18,7 +24,7 @@ public class PopUp extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PopUp frame = new PopUp();
+					PopUp frame = new PopUp("Some");
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -30,13 +36,29 @@ public class PopUp extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public PopUp() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
 
+	
+	public PopUp(String message) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 807, 462);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.DARK_GRAY);
+		contentPane.setForeground(Color.DARK_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Ok");
+		btnNewButton.setBounds(224, 313, 320, 21);
+		contentPane.add(btnNewButton);
+		
+		txtHelloOverThere = new JTextField();
+		txtHelloOverThere.setFont(new Font("Tahoma", Font.PLAIN, 16));
+		txtHelloOverThere.setHorizontalAlignment(SwingConstants.CENTER);
+		txtHelloOverThere.setText("<html>In following page you will have 3 options:<br/>- 32 Teams competiton<br/>-16 Teams competition<br/>-4 Teams competition</html>\r\n");
+		txtHelloOverThere.setBounds(22, 32, 748, 271);
+		contentPane.add(txtHelloOverThere);
+		txtHelloOverThere.setColumns(10);
+		txtHelloOverThere.setText(message);
+	}
 }
